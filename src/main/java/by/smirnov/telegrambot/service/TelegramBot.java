bf25315b03275872d2e988268f714fcfbdcfd99c
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-11-08 01:20:30 +0300

    Configured

---
+++ src/main/java/by/smirnov/telegrambot/service/TelegramBot.java
@@ -0,0 +1,23 @@
+package by.smirnov.telegrambot.service;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
